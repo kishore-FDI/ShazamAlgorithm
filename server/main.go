77dda84c2c8f409e2d6715550a19f43fd47eda92
Kishore <kishore22705@gmail.com>
2026-01-08 09:14:34 +0530

    Reworking the logic

--- server/main.go
+++ server/main.go
@@ -1,16 +1,18 @@
 package main
-import(
-	"fmt"
-	"ShazamAlgorithm/audioprocessing"
-)
-func main(){
-	fmt.Println("Hi Hello package main")
-	audioprocessing.ConvertStereoToMono("nodkrai.mp3","op.mp3")
-	sr , err := audioprocessing.GetSamplingRate("nodkrai.mp3")
-	if err==nil{
-		fmt.Println(sr)
 
-	} else {
-		fmt.Println("Error")
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	dir := "./audios"
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		fmt.Print(file)
 	}
 }
